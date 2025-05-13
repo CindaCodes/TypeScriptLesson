09f51f9aba3b33e6e9e2beb51b431a7363503278
cinda87 <j.c.bietz@gmail.com>
2025-05-13 07:53:56 -0600

    Lesson 04

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,64 @@
 "use strict";
+//Literal Types
+let myName;
+let userName;
+userName = "Jane"; // valid
+//functions
+const add = (a, b) => {
+    return a + b;
+};
+const logMsg = (message) => {
+    console.log(message);
+};
+logMsg("Hello");
+logMsg(add(2, 3));
+let subtract = function (c, d) {
+    return c - d;
+};
+//interface mathFunction {
+//  (a: number, b: number): number;};
+let multiply = function (c, d) {
+    return c * d;
+};
+logMsg(multiply(2, 2));
+// optional parameters
+const addAll = (a, b, c) => {
+    if (typeof c !== "undefined") {
+        return a + b + c;
+    }
+    return a + b;
+};
+const sumAll = (a = 10, b, c = 2) => {
+    return a + b + c;
+};
+logMsg(addAll(2, 3, 2));
+logMsg(addAll(2, 3));
+logMsg(sumAll(undefined, 3));
+//rest parameters
+const total = (a, ...nums) => {
+    return a + nums.reduce((prev, curr) => prev + curr, 0);
+};
+logMsg(total(10, 2, 3));
+const createError = (errMsg) => {
+    throw new Error(errMsg);
+};
+const infinite = () => {
+    let i = 1;
+    while (true) {
+        i++;
+        if (i > 100) {
+            // without this condition, it will run infinitely
+            break;
+        }
+    }
+};
+const isNumber = (value) => {
+    return typeof value === "number" ? true : false;
+};
+const numberOrString = (value) => {
+    if (typeof value === "string")
+        return "String";
+    if (typeof value === "number")
+        return "Number";
+    return createError("This should never happen");
+};
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,93 @@
 // Type Aliases
 type stringOrNumber = string | number;
 
-type stringOrNumberArray = stringOrNumber[];
+type stringOrNumberArray = (string | number)[];
 
 type Guitarist = {
   name?: string;
   active: boolean;
   albums: stringOrNumberArray;
-}
+};
 
-type UserId = stringOrNumber;
+type UserID = stringOrNumber;
 
+//Literal Types
+
+let myName: "Dave";
+let userName: "Dave" | "John" | "Jane";
+
+userName = "Jane"; // valid
+
+//functions
+const add = (a: number, b: number): Number => {
+  return a + b;
+};
+
+const logMsg = (message: any): void => {
+  console.log(message);
+};
+
+logMsg("Hello");
+logMsg(add(2, 3));
+
+let subtract = function (c: number, d: number): number {
+  return c - d;
+};
+
+type mathFunction = (a: number, b: number) => number;
+
+//interface mathFunction {
+//  (a: number, b: number): number;};
+
+let multiply: mathFunction = function (c, d) {
+  return c * d;
+};
+
+logMsg(multiply(2, 2));
+
+// optional parameters
+const addAll = (a: number, b: number, c?: number): number => {
+  if (typeof c !== "undefined") {
+    return a + b + c;
+  }
+  return a + b;
+};
+const sumAll = (a: number = 10, b: number, c: number = 2): number => {
+  return a + b + c;
+};
+
+logMsg(addAll(2, 3, 2));
+logMsg(addAll(2, 3));
+logMsg(sumAll(undefined, 3));
+
+//rest parameters
+const total = (a: number, ...nums: number[]): number => {
+  return a + nums.reduce((prev, curr) => prev + curr, 0);
+};
+
+logMsg(total(10, 2, 3));
+
+const createError = (errMsg: string): never => {
+  throw new Error(errMsg);
+};
+
+const infinite = () => {
+  let i: number = 1;
+  while (true) {
+    i++;
+    if (i > 100) {
+      // without this condition, it will run infinitely
+      break;
+    }
+  }
+};
+
+const isNumber = (value: any): boolean => {
+  return typeof value === "number" ? true : false;
+};
+
+const numberOrString = (value: number | string): string => {
+  if (typeof value === "string") return "String";
+  if (typeof value === "number") return "Number";
+  return createError("This should never happen");
+};
