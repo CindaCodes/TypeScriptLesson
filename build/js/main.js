7ecb87a66a563b43620a55988c7e993f8e2715b7
cinda87 <j.c.bietz@gmail.com>
2025-05-15 09:54:36 -0600

    Lesson 5

--- build/js/main.js
+++ build/js/main.js
@@ -1,64 +1,31 @@
 "use strict";
-//Literal Types
-let myName;
-let userName;
-userName = "Jane"; // valid
-//functions
-const add = (a, b) => {
-    return a + b;
+// type Casting or Assertion
+//convert to more or less specific
+let a = "hello";
+let b = a; // less specific
+let c = a; // more specific
+let d = "hello"; // less specific
+let e = "hello"; // less specific
+const addOrConcat = (a, b, c) => {
+    if (c === "add")
+        return a + b;
+    return "" + a + b;
 };
-const logMsg = (message) => {
-    console.log(message);
-};
-logMsg("Hello");
-logMsg(add(2, 3));
-let subtract = function (c, d) {
-    return c - d;
-};
-//interface mathFunction {
-//  (a: number, b: number): number;};
-let multiply = function (c, d) {
-    return c * d;
-};
-logMsg(multiply(2, 2));
-// optional parameters
-const addAll = (a, b, c) => {
-    if (typeof c !== "undefined") {
-        return a + b + c;
-    }
-    return a + b;
-};
-const sumAll = (a = 10, b, c = 2) => {
-    return a + b + c;
-};
-logMsg(addAll(2, 3, 2));
-logMsg(addAll(2, 3));
-logMsg(sumAll(undefined, 3));
-//rest parameters
-const total = (a, ...nums) => {
-    return a + nums.reduce((prev, curr) => prev + curr, 0);
-};
-logMsg(total(10, 2, 3));
-const createError = (errMsg) => {
-    throw new Error(errMsg);
-};
-const infinite = () => {
-    let i = 1;
-    while (true) {
-        i++;
-        if (i > 100) {
-            // without this condition, it will run infinitely
-            break;
-        }
-    }
-};
-const isNumber = (value) => {
-    return typeof value === "number" ? true : false;
-};
-const numberOrString = (value) => {
-    if (typeof value === "string")
-        return "String";
-    if (typeof value === "number")
-        return "Number";
-    return createError("This should never happen");
-};
+let myVal = addOrConcat(2, 2, "concat"); // type assertion
+// Be careful! TS will not throw an error - but a string is returned
+let nextVal = addOrConcat(2, 2, "concat");
+// 10 as string;
+// double assertion or double casting or forced casting
+10;
+// this is not recommended because it overrides the type system
+// use type guards instead
+// the DOM
+const img = document.querySelector("img"); // ! non-null assertion operator
+// the ! operator tells TS that the element will not be null
+// but this is not safe, because if the element is not found, it will throw an error
+// so we can use the optional chaining operator ? to check if the element is found
+const myImg = document.getElementById("#img");
+// error: Property 'src' does not exist on type 'Element | null'.
+const nextImg = document.getElementById("#img");
+img.src;
+myImg.src; // error: Property 'src' does not exist on type 'Element | null'.
--- src/main.ts
+++ src/main.ts
@@ -1,93 +1,47 @@
-// Type Aliases
-type stringOrNumber = string | number;
+// type Casting or Assertion
 
-type stringOrNumberArray = (string | number)[];
+type One = string;
+type Two = string | number;
+type Three = "hello";
 
-type Guitarist = {
-  name?: string;
-  active: boolean;
-  albums: stringOrNumberArray;
+//convert to more or less specific
+let a: One = "hello";
+let b = a as Two; // less specific
+let c = a as Three; // more specific
+
+let d = <One>"hello"; // less specific
+let e = <string | number>"hello"; // less specific
+
+const addOrConcat = (
+  a: number,
+  b: number,
+  c: "add" | "concat"
+): number | string => {
+  if (c === "add") return a + b;
+  return "" + a + b;
 };
 
-type UserID = stringOrNumber;
+let myVal: string = addOrConcat(2, 2, "concat") as string; // type assertion
 
-//Literal Types
+// Be careful! TS will not throw an error - but a string is returned
+let nextVal: number = addOrConcat(2, 2, "concat") as number;
 
-let myName: "Dave";
-let userName: "Dave" | "John" | "Jane";
+// 10 as string;
+// double assertion or double casting or forced casting
+10 as unknown as string;
+// this is not recommended because it overrides the type system
+// use type guards instead
 
-userName = "Jane"; // valid
+// the DOM
+const img = document.querySelector("img")!; // ! non-null assertion operator
+// the ! operator tells TS that the element will not be null
+// but this is not safe, because if the element is not found, it will throw an error
+// so we can use the optional chaining operator ? to check if the element is found
 
-//functions
-const add = (a: number, b: number): Number => {
-  return a + b;
-};
+const myImg = document.getElementById("#img") as HTMLImageElement;
+// error: Property 'src' does not exist on type 'Element | null'.
 
-const logMsg = (message: any): void => {
-  console.log(message);
-};
+const nextImg = <HTMLImageElement>document.getElementById("#img");
 
-logMsg("Hello");
-logMsg(add(2, 3));
-
-let subtract = function (c: number, d: number): number {
-  return c - d;
-};
-
-type mathFunction = (a: number, b: number) => number;
-
-//interface mathFunction {
-//  (a: number, b: number): number;};
-
-let multiply: mathFunction = function (c, d) {
-  return c * d;
-};
-
-logMsg(multiply(2, 2));
-
-// optional parameters
-const addAll = (a: number, b: number, c?: number): number => {
-  if (typeof c !== "undefined") {
-    return a + b + c;
-  }
-  return a + b;
-};
-const sumAll = (a: number = 10, b: number, c: number = 2): number => {
-  return a + b + c;
-};
-
-logMsg(addAll(2, 3, 2));
-logMsg(addAll(2, 3));
-logMsg(sumAll(undefined, 3));
-
-//rest parameters
-const total = (a: number, ...nums: number[]): number => {
-  return a + nums.reduce((prev, curr) => prev + curr, 0);
-};
-
-logMsg(total(10, 2, 3));
-
-const createError = (errMsg: string): never => {
-  throw new Error(errMsg);
-};
-
-const infinite = () => {
-  let i: number = 1;
-  while (true) {
-    i++;
-    if (i > 100) {
-      // without this condition, it will run infinitely
-      break;
-    }
-  }
-};
-
-const isNumber = (value: any): boolean => {
-  return typeof value === "number" ? true : false;
-};
-
-const numberOrString = (value: number | string): string => {
-  if (typeof value === "string") return "String";
-  if (typeof value === "number") return "Number";
-  return createError("This should never happen");
-};
+img.src;
+myImg.src; // error: Property 'src' does not exist on type 'Element | null'.
