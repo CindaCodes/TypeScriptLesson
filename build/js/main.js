4b3d33a9e0cb1a75a54ce448ce1635df9c8d3723
cinda87 <j.c.bietz@gmail.com>
2025-05-16 07:53:20 -0600

    Lesson 6

--- build/js/main.js
+++ build/js/main.js
@@ -1,31 +1,88 @@
 "use strict";
-// type Casting or Assertion
-//convert to more or less specific
-let a = "hello";
-let b = a; // less specific
-let c = a; // more specific
-let d = "hello"; // less specific
-let e = "hello"; // less specific
-const addOrConcat = (a, b, c) => {
-    if (c === "add")
-        return a + b;
-    return "" + a + b;
-};
-let myVal = addOrConcat(2, 2, "concat"); // type assertion
-// Be careful! TS will not throw an error - but a string is returned
-let nextVal = addOrConcat(2, 2, "concat");
-// 10 as string;
-// double assertion or double casting or forced casting
-10;
-// this is not recommended because it overrides the type system
-// use type guards instead
-// the DOM
-const img = document.querySelector("img"); // ! non-null assertion operator
-// the ! operator tells TS that the element will not be null
-// but this is not safe, because if the element is not found, it will throw an error
-// so we can use the optional chaining operator ? to check if the element is found
-const myImg = document.getElementById("#img");
-// error: Property 'src' does not exist on type 'Element | null'.
-const nextImg = document.getElementById("#img");
-img.src;
-myImg.src; // error: Property 'src' does not exist on type 'Element | null'.
+class Coder {
+    constructor(name, music, age, lang = "TypeScript" // default value makes it optional
+    ) {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
+    getAge() {
+        return `Hello, I am ${this.age}`;
+    }
+}
+const Dave = new Coder("Dave", "Rock", 42);
+console.log(Dave.getAge());
+// console.log(Dave.age);
+// console.log(Dave.lang);
+class WebDev extends Coder {
+    constructor(computer, name, music, age) {
+        super(name, music, age); // Call the parent constructor to super
+        this.computer = computer;
+        this.computer = computer;
+    }
+    // This makes Lang public and only accessible through this class
+    // If I was to console.log (Sara.lang) it would be undefined
+    getLang() {
+        return `I write ${this.lang}`;
+    }
+}
+const Sara = new WebDev("Mac", "Sara", "Lofi", 25);
+console.log(Sara.getLang());
+class Guitarist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action} the ${this.instrument}`;
+    }
+}
+const page = new Guitarist("Jimmy", "guitar");
+console.log(page.play("strums"));
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count; //++ makes sure increments to 1
+    }
+}
+Peeps.count = 0; // Static property
+const John = new Peeps("John");
+const Steve = new Peeps("Steve");
+const Amy = new Peeps("Amy");
+console.log(John.id); // 1
+console.log(Steve.id); // 2
+console.log(Amy.id); // 3
+console.log(Peeps.count);
+////////////////////////////////////////
+// Gitter and Setter
+class Bands {
+    constructor() {
+        this.dataState = [];
+    }
+    get data() {
+        return this.dataState;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every((el) => typeof el === "string")) {
+            this.dataState = value;
+            return;
+        }
+        else
+            throw new Error("Parameter is not an array of strings");
+    }
+}
+const MyBand = new Bands();
+MyBand.data = ["Metallica", "Guns N Roses", "Nirvana"];
+console.log(MyBand.data);
+MyBand.data = [...MyBand.data, "Queen"];
+console.log(MyBand.data);
+//MyBand.data = ['Van Halen', 5150] // Error: Parameter (5150) is not an array of strings
--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,128 @@
-// type Casting or Assertion
+class Coder {
+  // Need property in class and in constructor unless you add (public, private, protected..) in constructor
+  // name: string;
+  // music: string;
+  // age: number;
+  // lang: string;
 
-type One = string;
-type Two = string | number;
-type Three = "hello";
+  secondaryLang!: string; //! means it will be initialized later
 
-//convert to more or less specific
-let a: One = "hello";
-let b = a as Two; // less specific
-let c = a as Three; // more specific
+  constructor(
+    public readonly name: string,
+    public music: string,
+    private age: number,
+    protected lang: string = "TypeScript" // default value makes it optional
+  ) {
+    this.name = name;
+    this.music = music;
+    this.age = age;
+    this.lang = lang;
+  }
 
-let d = <One>"hello"; // less specific
-let e = <string | number>"hello"; // less specific
+  public getAge() {
+    return `Hello, I am ${this.age}`;
+  }
+}
 
-const addOrConcat = (
-  a: number,
-  b: number,
-  c: "add" | "concat"
-): number | string => {
-  if (c === "add") return a + b;
-  return "" + a + b;
-};
+const Dave = new Coder("Dave", "Rock", 42);
+console.log(Dave.getAge());
+// console.log(Dave.age);
+// console.log(Dave.lang);
 
-let myVal: string = addOrConcat(2, 2, "concat") as string; // type assertion
+class WebDev extends Coder {
+  constructor(
+    public computer: string,
+    name: string,
+    music: string,
+    age: number
+  ) {
+    super(name, music, age); // Call the parent constructor to super
+    this.computer = computer;
+  }
 
-// Be careful! TS will not throw an error - but a string is returned
-let nextVal: number = addOrConcat(2, 2, "concat") as number;
+  // This makes Lang public and only accessible through this class
+  // If I was to console.log (Sara.lang) it would be undefined
+  public getLang() {
+    return `I write ${this.lang}`;
+  }
+}
 
-// 10 as string;
-// double assertion or double casting or forced casting
-10 as unknown as string;
-// this is not recommended because it overrides the type system
-// use type guards instead
+const Sara = new WebDev("Mac", "Sara", "Lofi", 25);
+console.log(Sara.getLang());
+// console.log(Sara.lang); // undefined
+// console.log(Sara.age); // undefined
+//////////////////////////////////////////
 
-// the DOM
-const img = document.querySelector("img")!; // ! non-null assertion operator
-// the ! operator tells TS that the element will not be null
-// but this is not safe, because if the element is not found, it will throw an error
-// so we can use the optional chaining operator ? to check if the element is found
+// Interfaces
+interface Musician {
+  name: string;
+  instrument: string;
+  play(action: string): string;
+}
 
-const myImg = document.getElementById("#img") as HTMLImageElement;
-// error: Property 'src' does not exist on type 'Element | null'.
+class Guitarist implements Musician {
+  name: string;
+  instrument: string;
 
-const nextImg = <HTMLImageElement>document.getElementById("#img");
+  constructor(name: string, instrument: string) {
+    this.name = name;
+    this.instrument = instrument;
+  }
 
-img.src;
-myImg.src; // error: Property 'src' does not exist on type 'Element | null'.
+  play(action: string) {
+    return `${this.name} ${action} the ${this.instrument}`;
+  }
+}
+
+const page = new Guitarist("Jimmy", "guitar");
+console.log(page.play("strums"));
+
+class Peeps {
+  static count: number = 0; // Static property
+  static getCount(): number {
+    return Peeps.count;
+  }
+
+  public id: number;
+
+  constructor(public name: string) {
+    this.name = name;
+    this.id = ++Peeps.count; //++ makes sure increments to 1
+  }
+}
+
+const John = new Peeps("John");
+const Steve = new Peeps("Steve");
+const Amy = new Peeps("Amy");
+console.log(John.id); // 1
+console.log(Steve.id); // 2
+console.log(Amy.id); // 3
+
+console.log(Peeps.count);
+
+////////////////////////////////////////
+// Gitter and Setter
+class Bands {
+  private dataState: string[];
+
+  constructor() {
+    this.dataState = [];
+  }
+
+  public get data(): string[] {
+    return this.dataState;
+  }
+  public set data(value: string[]) {
+    if (Array.isArray(value) && value.every((el) => typeof el === "string")) {
+      this.dataState = value;
+      return;
+    } else throw new Error("Parameter is not an array of strings");
+  }
+}
+
+const MyBand = new Bands();
+MyBand.data = ["Metallica", "Guns N Roses", "Nirvana"];
+console.log(MyBand.data);
+MyBand.data = [...MyBand.data, "Queen"];
+console.log(MyBand.data);
+//MyBand.data = ['Van Halen', 5150] // Error: Parameter (5150) is not an array of strings
